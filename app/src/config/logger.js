3a2e4d6c82e3741e3032770b00e6fbdc27e3cb94
Pratik <101081557+pratxks@users.noreply.github.com>
2024-02-16 14:35:47 +0530

    Add logger utility for the backend

---
+++ app/src/config/logger.js
@@ -0,0 +1,16 @@
+// Simple logging utility
+const winston = require('winston');
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.timestamp(),
+        winston.format.printf(
+            ({ timestamp, level, message }) => `[${timestamp}] ${level}: ${message}`
+        )
+    ),
+    transports: [new winston.transports.Console()],
+});
+
+module.exports = logger;
